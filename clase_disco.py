f84a305963cd59b1d215c410c5c7138487f3e022
ceciliacolasanto <141371164+ceciliacolasanto@users.noreply.github.com>
2023-11-03 13:25:06 -0300

    Update clase_disco.py

--- clase_disco.py
+++ clase_disco.py
@@ -1,14 +1,14 @@
 class Disco:
 
-    def __init__(self, id, titulo, artista, precio, fecha, idioma, genero, n_canciones) -> None:
+    def __init__(self, id, titulo, artista, precio, fecha, idioma, n_canciones, genero) -> None:
         self.id = id
         self.titulo = titulo
         self.artista = artista
         self.precio = precio
         self.fecha = fecha
         self.idioma = idioma
-        self.genero = genero
         self._canciones = n_canciones
+        self.genero = genero
         
 
     def describe(self):
